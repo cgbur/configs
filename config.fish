22b4c2d408df09f9d3a533f42f83cbb4d7d31662
Chris Burgess <cgburgess10@gmail.com>
2022-06-25 12:38:51 -0400

    update shortcuts

--- config.fish
+++ config.fish
@@ -5,18 +5,18 @@ end
 set fish_function_path /home/cgbur/.config/fish/functions/theme-pure/functions/ $fish_function_path
 source /home/cgbur/.config/fish/functions/theme-pure/conf.d/pure.fish
 
-alias l="ls -lh"
-alias ll="ls -lah"
-alias c="clear"
-alias vim="nvim"
-alias ct="cargo t"
+alias c="cargo"
 alias g="git"
 alias gl="git log --oneline"
 alias gs="git status"
-alias c="cargo"
 alias lab="jupyter lab --no-browser --port 8080"
-alias p="python3"
+alias ll="ls -lah"
+alias l="ls -lh"
 alias nf="numfmt --to=iec-i --suffix B"
+alias p="python3"
+alias vim="nvim"
+alias v="vim"
+alias x="clear"
 
 function s
     python3 -m http.server 8080
